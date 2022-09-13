1d821a46441f2df1f461f3cfe6d75de20972ad6c
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-13 14:17:12 -0300

    feat: (default App.js) implement GlobalContext.js, GlobalStyle.sc.js and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,24 @@
+import {useState} from "react";
+import {ThemeProvider} from "styled-components";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {GlobalStyle} from "./styles/GlobalStyle.sc";
+import GlobalContext from "../contexts/GlobalContext";
+
 export default function App() {
-  return <h1>Hello World!</h1>;
+  const [profile, setProfile] = useState(null);
+
+  const theme = {
+    water: {button: "blue"},
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <GlobalContext.Provider value={{profile, setProfile}}>
+        <BrowserRouter>
+          <Routes>{/*TODO: add route*/}</Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
+    </ThemeProvider>
+  );
 }
