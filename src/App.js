dec5a33d968289afc63f5ea7700ded0e23f0a368
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-14 00:46:25 -0300

    feat: (theme colors) add theme colors for each pokemon type + the logo.png

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GlobalStyle } from "./assets/styles/GlobalStyle.sc.js";
+import {useState} from "react";
+import {ThemeProvider} from "styled-components";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {GlobalStyle} from "./assets/styles/GlobalStyle.sc.js";
 import GlobalContext from "./tools/GlobalContext";
-
 import LoginPage from "./components/LoginPage.js";
+import {theme} from "./tools/Theme";
 
 export default function App() {
   const [profile, setProfile] = useState(null);
 
-  const theme = {
-    water: { button: "blue" },
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <GlobalContext.Provider value={{ profile, setProfile }}>
+      <GlobalContext.Provider value={{profile, setProfile}}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginPage />} />;
             {/* <Route path="/signup" element={<SignUpPage />} />; */}
           </Routes>
-
         </BrowserRouter>
       </GlobalContext.Provider>
     </ThemeProvider>
---
+++ src/tools/Theme.js
@@ -0,0 +1,49 @@
+export const theme = {
+  white: "#fafafa",
+  black: "#333",
+  grass: {
+    light: "#67de85",
+    medium: "#2DC653",
+    dark: "#208B3A",
+  },
+  fire: {
+    light: "#ff6d62",
+    medium: "#f85144",
+    dark: "#de3d30",
+  },
+  water: {
+    light: "#48cae4",
+    medium: "#00b4d8",
+    dark: "#0077b6",
+  },
+  normal: {
+    light: "#4461f1",
+    medium: "#2648F1",
+    dark: "#112ebe",
+  },
+  poison: {
+    light: "#a354e6",
+    medium: "#7b2cbf",
+    dark: "#5a189a",
+  },
+  eletric: {
+    light: "#fae260",
+    medium: "#ffd53e",
+    dark: "#fdb833",
+  },
+  ground: {
+    light: "#a56336",
+    medium: "#815839",
+    dark: "#5b412d",
+  },
+  psychic: {
+    light: "#fb718c",
+    medium: "#f85777",
+    dark: "#fb385e",
+  },
+  bug: {
+    light: "#c9e860",
+    medium: "#A5C828",
+    dark: "#7f9b1b",
+  },
+};
