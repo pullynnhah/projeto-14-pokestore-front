b75d26364f6d2e888f09f82e34e6f653f4518c46
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-16 17:01:55 -0300

    refactor: creat TypeBar Component

---
+++ src/components/PokemonBar.js
@@ -0,0 +1,91 @@
+import typeFinder from "../tools/typeFinder";
+import {useState} from "react";
+import {getPokemons} from "../tools/UseAxios";
+import styled from "styled-components";
+
+export default function PokemonBar({authorized, unauthorized}) {
+  const [type, setType] = useState("default");
+  const [typeSelections, setTypeSelections] = useState([
+    {type: "all", selected: "none"},
+    {type: "grass", selected: "none"},
+    {type: "electric", selected: "none"},
+    {type: "fire", selected: "none"},
+    {type: "water", selected: "none"},
+    {type: "normal", selected: "none"},
+    {type: "poison", selected: "none"},
+    {type: "ground", selected: "none"},
+    {type: "psychic", selected: "none"},
+    {type: "bug", selected: "none"},
+    {type: "legendary", selected: "none"},
+    {type: "others", selected: "none"},
+  ]);
+
+  function selectType(index) {
+    const selectedBefore = typeSelections.find(typeSelection => typeSelection.selected === "solid");
+    if (selectedBefore !== undefined) {
+      selectedBefore.selected = "none";
+    }
+    const selections = [...typeSelections];
+    selections[index].selected = "solid";
+    setTypeSelections([...selections]);
+
+    const selectedType = typeSelections[index].type;
+    setType(typeFinder(selectedType));
+    const promise = getPokemons({type: selectedType});
+    promise.then(authorized);
+    promise.catch(unauthorized);
+  }
+
+  return (
+    <FilterBar type={type}>
+      {typeSelections.length === 0
+        ? ""
+        : typeSelections.map((typeSelection, index) => (
+            <Button
+              selected={typeSelection.selected}
+              onClick={() => {
+                selectType(index);
+              }}
+              type={typeFinder(typeSelection.type)}>
+              {typeSelection.type}
+            </Button>
+          ))}
+    </FilterBar>
+  );
+}
+
+const FilterBar = styled.div`
+  height: auto;
+  background: linear-gradient(
+    ${props => props.theme[props.type].dark},
+    ${props => props.theme[props.type].medium},
+    ${props => props.theme[props.type].dark}
+  );
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  overflow-x: auto;
+  padding: 20px 10px 10px 10px;
+`;
+const Button = styled.div`
+  min-width: 100px;
+  width: 100px;
+  height: 45px;
+  background: linear-gradient(
+    ${props => props.theme[props.type].light},
+    ${props => props.theme[props.type].dark},
+    ${props => props.theme[props.type].light}
+  );
+  border: 3px ${props => props.selected} ${props => props.theme[props.type].dark};
+  display: flex;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  color: ${props => props.theme.white};
+  font-weight: 800;
+  text-transform: upper;
+  box-shadow: 3px 3px 3px 0 rgba(0, 0, 0, 0.5);
+`;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,56 +3,25 @@ import Footer from "../commons/Footer";
 import styled from "styled-components";
 import {useEffect, useState} from "react";
 import {getPokemons} from "../../tools/UseAxios";
-import {useNavigate} from "react-router-dom";
 import Pokemon from "../Pokemon";
-import typeFinder from "../../tools/typeFinder";
 import Loader from "../commons/Loader";
+import PokemonBar from "../PokemonBar";
 
-export default function HomePage({children, type}) {
-  const navigate = useNavigate();
+export default function HomePage() {
   const [pokemons, setPokemons] = useState(null);
-  const [homeType, setHomeType] = useState(type);
-  const [typeSelections, setTypeSelections] = useState([
-    {type: "all", selected: "none"},
-    {type: "grass", selected: "none"},
-    {type: "electric", selected: "none"},
-    {type: "fire", selected: "none"},
-    {type: "water", selected: "none"},
-    {type: "normal", selected: "none"},
-    {type: "poison", selected: "none"},
-    {type: "ground", selected: "none"},
-    {type: "psychic", selected: "none"},
-    {type: "bug", selected: "none"},
-    {type: "legendary", selected: "none"},
-    {type: "others", selected: "none"},
-  ]);
+  const [type, setType] = useState("default");
 
   useEffect(() => {
-    const pokeType = {type: "all"};
-    const promise = getPokemons(pokeType);
-    promise.then(autorized);
-    promise.catch(unautorized);
+    const promise = getPokemons({type: "all"});
+    promise.then(authorized);
+    promise.catch(unauthorized);
   }, []);
 
-  function selectType(index) {
-    const selectedBefore = typeSelections.find(typeSelection => typeSelection.selected === "solid");
-    if (selectedBefore !== undefined) {
-      selectedBefore.selected = "none";
-    }
-    typeSelections[index].selected = "solid";
-    setTypeSelections([...typeSelections]);
-    setHomeType(typeFinder(typeSelections[index].type));
-    const pokeType = {type: typeSelections[index].type};
-    const promisse = getPokemons(pokeType);
-    promisse.then(autorized);
-    promisse.catch(unautorized);
-  }
-
-  function autorized(response) {
+  function authorized(response) {
     setPokemons(response.data);
   }
 
-  function unautorized(error) {
+  function unauthorized(error) {
     if (error.response.data === undefined) {
       alert("Error: unable to connect to server");
     } else {
@@ -61,22 +30,9 @@ export default function HomePage({children, type}) {
   }
 
   return (
-    <Wrapper type={homeType}>
-      <Header type={homeType} />
-      <FilterBar type={homeType}>
-        {typeSelections.length === 0
-          ? ""
-          : typeSelections.map((typeSelection, index) => (
-              <Button
-                selected={typeSelection.selected}
-                onClick={() => {
-                  selectType(index);
-                }}
-                type={typeFinder(typeSelection.type)}>
-                {typeSelection.type}
-              </Button>
-            ))}
-      </FilterBar>
+    <Wrapper type={type}>
+      <Header type={type} />
+      <PokemonBar type={type} authorized={authorized} unauthorized={unauthorized} />
       <Pokemons>
         {!pokemons ? (
           <Loader />
@@ -84,7 +40,7 @@ export default function HomePage({children, type}) {
           pokemons.map(pokemon => <Pokemon key={pokemon._id} {...pokemon} />)
         )}
       </Pokemons>
-      <Footer type={homeType} />
+      <Footer type={type} />
     </Wrapper>
   );
 }
@@ -98,41 +54,7 @@ const Wrapper = styled.div`
     )
     fixed;
 `;
-const FilterBar = styled.div`
-  height: auto;
-  background: linear-gradient(
-    ${props => props.theme[props.type].dark},
-    ${props => props.theme[props.type].medium},
-    ${props => props.theme[props.type].dark}
-  );
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  overflow-x: auto;
-  padding: 20px 10px 10px 10px;
-`;
-const Button = styled.div`
-  min-width: 100px;
-  width: 100px;
-  height: 45px;
-  background: linear-gradient(
-    ${props => props.theme[props.type].light},
-    ${props => props.theme[props.type].dark},
-    ${props => props.theme[props.type].light}
-  );
-  border: 3px ${props => props.selected} ${props => props.theme[props.type].dark};
-  display: flex;
-  border-radius: 10px;
-  justify-content: center;
-  align-items: center;
-  margin-left: 10px;
-  margin-bottom: 10px;
-  cursor: pointer;
-  color: ${props => props.theme.white};
-  font-weight: 800;
-  text-transform: upper;
-  box-shadow: 3px 3px 3px 0 rgba(0, 0, 0, 0.5);
-`;
+
 const Pokemons = styled.div`
   width: 100%;
   height: 100%;
