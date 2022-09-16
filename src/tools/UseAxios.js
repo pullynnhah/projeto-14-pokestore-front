4aee036a480f9e03e4aa65cc3f728a237406654c
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-16 18:30:04 -0300

    feat/refactor: create my axios functions + refactor useAxios a little

--- src/tools/UseAxios.js
+++ src/tools/UseAxios.js
@@ -4,50 +4,71 @@ import dotenv from "dotenv";
 dotenv.config();
 
 function Login(body) {
-  const promise = axios.post(`${process.env.REACT_APP_API_URI}/login`, body);
-  return promise;
-};
-
-// TODO: add validation
-function getPokemons(body) {
-    const config = {
-        headers: {
-
-        }
-    };
-    const promise = axios.get(`${process.env.REACT_APP_API_URI}/pokemons?type=${body.type}`, config);
-    return promise;
-};
-
-function SignUp(body) {
-    const promise = axios.post(`${process.env.REACT_APP_API_URI}/signup`, body);
-    return promise;
+  return axios.post(`${process.env.REACT_APP_API_URI}/login`, body);
 }
 
-// TODO: add validation
+function getPokemons(body) {
+  return axios.get(`${process.env.REACT_APP_API_URI}/pokemons?type=${body.type}`);
+}
+
+function SignUp(body) {
+  return axios.post(`${process.env.REACT_APP_API_URI}/signup`, body);
+}
+
 function getPokemon(pokedexNumber) {
-    console.log(process.env.REACT_APP_API_URI);
-    return axios.get(`${process.env.REACT_APP_API_URI}/pokemon/${pokedexNumber}`);
+  return axios.get(`${process.env.REACT_APP_API_URI}/pokemon/${pokedexNumber}`);
 }
 
 function getHistory(body) {
-    const config = {
-        headers: {
-            userid:body
-        }
-    }
-    const promise = axios.get(`${process.env.REACT_APP_API_URI}/history`, config);
-    return promise;
+  const config = {
+    headers: {
+      userid: body,
+    },
+  };
+  return axios.get(`${process.env.REACT_APP_API_URI}/history`, config);
 }
 
 function Logout(profile) {
   const config = {
     headers: {
-      "user": profile.userId
-    }
-  }
-  const promise = axios.delete(`${process.env.REACT_APP_API_URI}/logout`, config);
-  return promise;
-};
+      user: profile.userId,
+    },
+  };
+  return axios.delete(`${process.env.REACT_APP_API_URI}/logout`, config);
+}
 
-export { SignUp, Login, getPokemon, getPokemons, getHistory, Logout };
+function listCart(profile, mode) {
+  const config = {
+    headers: {
+      user: profile.userId,
+    },
+    params: {mode},
+  };
+
+  return axios.get(`${process.env.REACT_APP_API_URI}/cart`, config);
+}
+
+function delCartItem(profile) {
+  return axios.post(`${process.env.REACT_APP_API_URI}/cart/${profile.userId}`);
+}
+
+function checkout(profile) {
+  const config = {
+    headers: {
+      user: profile.userId,
+    },
+  };
+  return axios.post(`${process.env.REACT_APP_API_URI}/checkout`, config);
+}
+
+export {
+  SignUp,
+  Login,
+  getPokemon,
+  getPokemons,
+  getHistory,
+  Logout,
+  listCart,
+  delCartItem,
+  checkout,
+};
