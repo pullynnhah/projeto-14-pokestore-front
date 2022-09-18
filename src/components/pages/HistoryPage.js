dfe47200ec99ec4b17414936d4ef8f21a0e09ec6
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-18 03:24:21 -0300

    bug: there is a bug on the backend that needs fixing

--- src/components/pages/HistoryPage.js
+++ src/components/pages/HistoryPage.js
@@ -1,113 +1,138 @@
-import Page from "../commons/Page.js";
 import styled from "styled-components";
-import { useEffect, useState } from "react";
-import { getHistory, listCart } from "../../tools/UseAxios";
+import {useContext, useEffect, useState} from "react";
+import {listCart} from "../../tools/UseAxios";
+import GlobalContext from "../../tools/GlobalContext";
+import Header from "../commons/Header";
+import Footer from "../commons/Footer";
+import Loader from "../commons/Loader";
 
+export default function HistoryPage({type}) {
+  const [history, setHistory] = useState(null);
 
-export default function HistoryPage({ children, type }) {
+  const {profile} = useContext(GlobalContext);
 
-    const [history, setHistory] = useState([]);
+  useEffect(() => {
+    const promisse = listCart(profile, "history");
+    promisse.then(authorized);
+    promisse.catch(unauthorized);
+  }, []);
 
+  function authorized(response) {
+    const data = response.data;
+    data.forEach(datum => {
+      const images = [];
+      for (let i = 0; i < datum.quantity; i++) {
+        images.push(datum.image);
+      }
+      data.images = images;
+    });
+    setHistory(data);
+  }
 
-    useEffect(() => {
-        const user = {userId:"63229691a2e99a22734583bd"}; //modificar para user logado
-        const promisse = listCart(user,"history");
-        promisse.then(autorized);
-        promisse.catch(unautorized);
-    }, [])
-
-    function autorized(response) {
-        const data = response.data;
-        for(let i=0;i<data.length;i++){
-            data[i].imgLength = [];
-            for(let j=0;j<data[i].quantity;j++){
-                data[i].imgLength.push(data[i].image);
-            }
-        }
-        setHistory(response.data);
-        console.log(response.data)
+  function unauthorized(error) {
+    if (error.response.data === undefined) {
+      alert("Error: unhable to connect to server");
+    } else {
+      alert(error.response.data);
     }
+  }
 
-    function unautorized(error) {
-        if (error.response.data === undefined) {
-            alert('Error: unhable to connect to server');
-        }
-        else {
-            alert(error.response.data);
-        }
-    }
-
+  if (!history) {
     return (
-        <Page type={type}>
-            <Wrapper type={type}>
-                History of user:
-                <HistoryContainer>
-                    {history.length === 0 ? '' : history.map(hist => 
-                    <PurchaseHistory type={hist.type}>
-                        <div>ID: {hist.cartId}</div>
-                        <div>Purchase Date: {hist.purchaseDate}</div>
-                        <div>Delivery Date: {hist.deliveryDate}</div>
-                        <div>Pokemon: {hist.name}</div>
-                        <div>Quantity: {hist.quantity}</div>
-                        <div>Total price: ${(hist.quantity*hist.price).toFixed(2)}</div>
-                        {
-                            <div>{hist.imgLength.map((img) => (<img src={img}/>))} </div>
-                        }
-                       
-                    </PurchaseHistory> )}
-                </HistoryContainer>
-            </Wrapper>
-        </Page>
+      <>
+        <Header type={type} />
+        <Loader type={type} />
+        <Footer type={type} />
+      </>
     );
+  }
+
+  return (
+    <>
+      <Header type={type} />
+      <Wrapper type={type}>
+        History of user:
+        <HistoryContainer>
+          {history.map(({cartId, purchaseDate, deliveryDate, name, quantity, price, images}) => (
+            <PurchaseHistory type={type}>
+              <p>ID: {cartId}</p>
+              <p>Purchase Date: {purchaseDate}</p>
+              <p>Delivery Date: {deliveryDate}</p>
+              <p>Pokemon: {name}</p>
+              <p>Quantity: {quantity}</p>
+              <p>Unit price: $ {price}.toFixed(2)</p>
+              <h2>Total price: $ {(quantity * price).toFixed(2)}</h2>
+              {
+                <div>
+                  {images.map(img => (
+                    <img src={img} alt={name} />
+                  ))}{" "}
+                </div>
+              }
+            </PurchaseHistory>
+          ))}
+        </HistoryContainer>
+      </Wrapper>
+      <Footer type={type} />
+    </>
+  );
 }
 
 const Wrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    color: ${props => props.theme.pokemonBlue};
-    font-size: 30px;
-    font-weight: 800;
-`
+  width: 100%;
+  height: 100%;
+  color: ${props => props.theme.pokemonBlue};
+  font-size: 30px;
+  font-weight: 800;
+  background: linear-gradient(
+    ${props => props.theme[props.type].light},
+    ${props => props.theme[props.type].lighter}
+  );
+`;
 const HistoryContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    margin-top: 25px;
-`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 25px;
+`;
 const PurchaseHistory = styled.div`
-    width: 400px;
-    height: auto;
-    background: linear-gradient( 45deg,
-        ${props => props.theme[props.type].light},
-        ${props => props.theme[props.type].lighter},
-        ${props => props.theme[props.type].light}
-        );
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 10px;
-    margin: 15px;
+  width: 400px;
+  height: auto;
+  background: linear-gradient(
+    45deg,
+    ${props => props.theme[props.type].light},
+    ${props => props.theme[props.type].lighter},
+    ${props => props.theme[props.type].light}
+  );
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10px;
+  margin: 15px;
+  box-shadow: 3px 3px 3px 0 rgba(0, 0, 0, 0.5);
+
+  div {
+    color: ${props => props.theme[props.type].dark};
+    font-size: 15px;
+    background: linear-gradient(
+      to right,
+      ${props => props.theme[props.type].light},
+      ${props => props.theme[props.type].lighter},
+      ${props => props.theme[props.type].light}
+    );
+    border-radius: 5px;
+    margin-bottom: 8px;
     box-shadow: 3px 3px 3px 0 rgba(0, 0, 0, 0.5);
-    div{
-        color: ${props => props.theme[props.type].dark};
-        font-size: 15px;
-        background:  linear-gradient( to right,
-            ${props => props.theme[props.type].light},
-            ${props => props.theme[props.type].lighter},
-            ${props => props.theme[props.type].light}
-            );
-        border-radius: 5px;
-        margin-bottom: 8px;
-        box-shadow: 3px 3px 3px 0 rgba(0, 0, 0, 0.5);
-        overflow: auto;
-        padding: 3px;
-    }
-    img{
-        width: 60px;
-        border-radius: 10px;
-        margin: 5px;
-    }
-`
+    overflow: auto;
+    padding: 3px;
+  }
+
+  img {
+    width: 60px;
+    border-radius: 10px;
+    margin: 5px;
+  }
+`;
